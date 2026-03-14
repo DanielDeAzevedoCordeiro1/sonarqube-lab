5b78db3f15fc8f9ee7d6de80cca24e9e43d9dfc9
Daniel <daniel.cordeiro2@fatec.sp.gov.br>
2026-03-14 15:41:30 -0300

    ✨ feat: Adiciona dominio da aplicacao

---
+++ src/main/java/com/example/todo/domain/model/Todo.java
@@ -0,0 +1,48 @@
+package com.example.todo.domain.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "todos")
+public class Todo {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @NotBlank
+  private String title;
+
+  private boolean done;
+
+  protected Todo() { }
+
+  public Todo(String title) {
+    this.title = title;
+    this.done = false;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public boolean isDone() {
+    return done;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public void setDone(boolean done) {
+    this.done = done;
+  }
+}
